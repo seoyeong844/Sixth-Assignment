3013565a4d9c2e3d4769d5d8885da153a6611ae4
seoyeong844 <ruccy3007@naver.com>
2021-05-23 15:21:51 +0900

    sixth assignment

--- fifth/fifthProject/settings.py
+++ fifth/fifthProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'diaryapp',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'fifthProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['fifthproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
